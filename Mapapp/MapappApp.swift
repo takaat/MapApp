8169864245f01ca85207d2846578fe399234f287
mana.u <great8life@gmail.com>
2021-12-04 14:42:07 +0900

    Initial Commit

---
+++ Mapapp/MapappApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapappApp.swift
+//  Mapapp
+//
+//  Created by 上田学 on 2021/12/04.
+//
+
+import SwiftUI
+
+@main
+struct MapappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
